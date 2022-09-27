a3726ef5bd3def4bbac5afbd44b6b7bb7cb18048
faniseptinaayu <114465223+faniseptinaayu@users.noreply.github.com>
2022-09-27 12:50:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Projek PKK</title>
+</head>
+<body>
+    <div class="nav">
+    <div class="menu-kiri">
+        <h2>Nama Web</h2>
+</div>
+<div class="menu-kanan">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li>Produk</li>
+        <li>Menu 3</li>
+
+</ul>
+</div>
+</div>
+<hr>
+<br>
+<h1>ANGGOTA KELOMPOK</h1>
+<table border="1">
+    <tr>
+        <th>foto</th>
+        <th>biodata</th>
+</body>
+
+<tr>
+    <td> <img src="fan.jpg" width="50"><br></td>
+    <td> NAMA : FANI <br> TTL : TSM,17-O9-2005 <br> ALAMAT : KP.CIMERAK</td>
+</tr>
+
+
+<tr>
+    <td> <img src="fan.jpg" width="50"><br></td>
+    <td> NAMA : ASRI <br> TTL : TSM,13-O6-2005 <br> ALAMAT : KP.GOLEMPANG</td>
+</tr>
+
+
+<tr>
+    <td> <img src="fan.jpg" width="50"><br></td>
+    <td> NAMA : EGA <br> TTL : TSM,1-10-2005 <br> ALAMAT : KP.CIMERAK</td>
+</tr>
---
+++ produk.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>projek pkk</title>
+</head>
+<body>
+<div class="nav">
+    <div class="menu-kiri">
+        <h2>Nama Web</h2>
+</div>
+<div class="menu-kanan">
+    <ul>
+        <li><a href="indek.php">HOME</a></li>
+        <li><a href="index.php">PRODUK</a></li>
+</ul>
+</ul>
+
+   </div>
+</div>
+<hr>
+<br>
+<h1>PRODUK</h1>
+<table border="1">
+    <tr>
+</body>
+
+<tr>
+    <td> <img src="Basreng.webp" width="50">
+    <br><p>Nama produk : Basreng Mantul</p>
+    <p>Harga : 5k</p>
+    <p>Deskripsi : Basreng adalah cemilan khas sunda yang di iris tipis kemudian di goreng.<br>
+      Basreng dapat juga disajikan dengan taburan bumbu pedas.</p>
+      <br>
+      <button>PESAN</button>
+</td>
+</ul>
+   </div>
+<hr>
+<br>
+<table border="1">
+    <tr>
+</body>
+<tr>
+    <td> <img src="getuk.jpg" width="50">
+    <br><p>Nama produk : Getuk</p>
+    <p>Harga : 3k</p>
+    <p>Deskripsi : Getuk adalah makanan ringan yang terbuat dari bahan singkong.<br>
+      Dengan di campur gula merah dan di taburi parutan kelapa.</p>
+      <br>
+      <button>PESAN</button>
+</td>
+</ul>
+
+   </div>
+<hr>
+<br>
+<table border="1">
+    <tr>
+</body>
+
+<tr>
+    <td> <img src="telurgulung.jpg" width="50">
+    <br><p>Nama produk : Telur gulung</p>
+    <p>Harga : 1k</p>
+    <p>Deskripsi : Telur gulng adalah makanan tradisional dan merupakan variasi dari telur goreng,<br>
+      yang di mana sebuah telur akan digoreng,</p>
+      kemudian digulung menggunakan sebuah tusukan yang biasanya di buat dari kayu pohon bambu.
+      <br>
+      <button>PESAN</button>
+</br>
+</td>
+</tr>
+
+
+
+
+
+
+  
